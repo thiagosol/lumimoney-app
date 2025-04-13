7e0cf38bb3bd6bae2fa970c6cd98788b7ab2a760
Thiago Sol <thiagosol@ymail.com>
2025-04-12 21:33:59 -0300

    Update env.dart

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,5 +1,6 @@
 class Env {
-  static const String _defaultApiBaseUrl = 'https://api.lumimoney.thiagosol.com';
+  static const String _defaultApiBaseUrl =
+      'https://api-lumimoney.thiagosol.com';
   static String get apiBaseUrl => const String.fromEnvironment(
         'API_BASE_URL',
         defaultValue: _defaultApiBaseUrl,
